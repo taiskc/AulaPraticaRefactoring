b2c51f7d2d42b27859ae2766922137cbd0484187
Taís Christofani <45609806+taiskc@users.noreply.github.com>
2022-02-05 22:46:57 +0000

    Commit 4

--- Rental.java
+++ Rental.java
@@ -15,4 +15,24 @@ public class Rental {
    public Movie getMovie() {
       return _movie;
    }
+
+   public double getCharge() {
+    double thisAmount = 0;
+    switch (this._movie.getPriceCode()) {
+          case Movie.REGULAR:
+            thisAmount += 2;
+            if (this._daysRented > 2)
+                thisAmount += (this._daysRented - 2) * 1.5;
+            break;
+          case Movie.NEW_RELEASE:
+            thisAmount += this._daysRented * 3;
+            break;
+          case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (this._daysRented > 3)
+                thisAmount += (this._daysRented - 3) * 1.5;
+              break;
+      }
+      return thisAmount;
+   }
 }
